70014fab08c242ce9ab48478daba40db26d4f027
Sean Goresht <seangores@gmail.com>
2020-11-07 01:16:15 -0700

    feat: trigger submit callback if specified

--- addon/components/former.js
+++ addon/components/former.js
@@ -1,4 +1,5 @@
 import Component from "@glimmer/component";
+import {action} from "@ember/object";
 
 export const CLASS_NAMES = {
 	FORM: "former"
@@ -8,4 +9,11 @@ export const ERROR_MESSAGES = {
 	NO_MODEL_SUPPLIED_MESSAGE: "No @model argument provided.  Please provide a @model to the <Former> component"
 };
 
-export default class FormerComponent extends Component {}
+export default class FormerComponent extends Component {
+	@action onSubmit(submitEvent) {
+		submitEvent.preventDefault();
+		if (this.args.onSubmit) {
+			this.args.onSubmit(...arguments);
+		}
+	}
+}
